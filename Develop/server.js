3ce63507ac0d73e1c0e46d973e020add14b481b2
jrwesch <jrwesch@gmail.com>
2023-04-05 15:33:42 -0400

    get to return saved JSON notes, but need to post notes first

--- Develop/server.js
+++ Develop/server.js
@@ -5,7 +5,7 @@ const express = require('express');
 const PORT = process.env.PORT || 3001;
 const app = express();
 
-const dbData = require('./db/db.json');
+const db = require('./db/db.json');
 
 // unique ID for all notes
 const { v4: uuidv4 } = require('uuid');
@@ -15,20 +15,28 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(express.static('public'));
 
+// read the db.json file and return saved notes as JSON
+app.get('/api/notes', (req, res) => {
+    fs.readFile('./db/db.json', (err, data) => {
+        if (err) throw err;
+        let dbData = JSON.parse(data);
+        res.json(dbData)
+    });
+});
 
 // Notes Route
 app.get('/notes', (req, res) => {
-    res.sendFile(path.join(_dirname, './public/notes.html'));
+    res.sendFile(path.join(__dirname, './public/notes.html'));
 });
 
 // Home Route
 app.get('/', (req, res) => {
-    res.sendFile(path.join(_dirname, './public/index.html'));
+    res.sendFile(path.join(__dirname, './public/index.html'));
 });
 
 // Wildcard Rout
 app.get('*', (req,res) => {
-    res.sendFile(path.join(_dirname, './public/index.html'));
+    res.sendFile(path.join(__dirname, './public/index.html'));
 });
 
 
